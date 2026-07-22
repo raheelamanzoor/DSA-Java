f269c87a1df4f973eaecd9cc91237e7757d42396
raheelamanzoor <syedlailah@gmail.com>
2026-07-22 22:25:58 +0530

    Solved basic Java problems using loops and conditionals

---
+++ Basics/ForLoop.java
@@ -0,0 +1,10 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 9; i++)
+        {
+            System.out.println(i);
+        }
+        System.out.println("done");
+    }
+    
+}
