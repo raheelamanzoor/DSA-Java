f05062c7b72f81ab2629ecedb8b03348f2ef6734
raheelamanzoor <syedlailah@gmail.com>
2026-08-14 15:54:03 +0530

    Input handling, prime numbers, and Fibonacci numbers

---
+++ Basics/Input.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Input{
+    public static void main(String[] args){
+
+        /*Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        for( int i = 0; i<=n; i++)
+        {
+            System.out.println(i);
+        }*/
+
+        /*  Scanner scn = new Scanner(System.in);
+        String name = scn.nextLine();
+        
+        {
+            System.out.println("hello " + name);
+        } */
+       
+            Scanner scn = new Scanner(System.in);
+            int n = Integer.parseInt(scn.nextLine());
+            String name = scn.nextLine();
+            System.out.println("Dear " + name + ". Here is counting:");
+            for(int i=0; i<=n; i++ )
+            {
+                System.out.println(i);
+            }
+    }
+}
