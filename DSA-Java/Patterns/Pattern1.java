4b15e1e7e0b2fc6516fbb1d2a5fb6d0e8f21e7dd
raheelamanzoor <syedlailah@gmail.com>
2026-08-31 20:26:29 +0530

    Add Patterns: Pattern13, Pattern14 and Pattern15

--- DSA-Java/Patterns/Pattern1.java
+++ DSA-Java/Patterns/Pattern1.java
@@ -1,6 +1,6 @@
 package Patterns;
 import java.util.*;
-public class Triangle{
+public class Pattern1{
     public static void main (String[] args){
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
--- DSA-Java/Patterns/Pattern2.java
+++ DSA-Java/Patterns/Pattern2.java
@@ -1,6 +1,6 @@
 package Patterns;
 import java.util.*;
-public class InvertedTriangle {
+public class Pattern2 {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
